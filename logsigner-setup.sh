3ba2042c21d1f439761ab67388d58e09ad0c52f2
mt190592 <mt190502@mtaha.dev>
2023-08-27 11:41:53 +0300

    conflict prevented on monospot installation after logbrowser

--- logsigner-setup.sh
+++ logsigner-setup.sh
@@ -5,39 +5,44 @@
 [ ! -e $PWD/$CONFIGFILE ] && { echo "logsigner-setup.conf dosyasi bulunamadi, git uzerinden cekilecek"; fetch https://raw.githubusercontent.com/monobilisim/pfsense-5651/master/logsigner-setup.conf; echo "Dosyayi bir editor araciligi ile duzenledikten sonra logsigner-setup.sh'i yeniden calistirin"; exit 0; } 
 . $PWD/$CONFIGFILE
 
-#~ remove old configs
-[ "$REM_OLD_CFG" == "true" ] && { echo "Eski yapılandırmalar ve dosyalar siliniyor..."; rm -rf /logimza/.openssl /logimza/$(date +%Y) /usr/local/www/log_browser /sbin/logsigner.sh /sbin/logimza* /sbin/dhcptibduzenle.sh /sbin/dhcpdmodify.awk; } || { [ -e /sbin/logsigner.sh ] && { echo "Logsigner zaten kurulu, yeniden kurmak icin $CONFIGFILE dosyasi icinden REM_OLD_CFG degerini true yapin..."; exit 1; }; }
-
 #~ save project directory
 PROJECT_DIRECTORY=$PWD
 
-#~ make new folder and write config
-mkdir -p /logimza/.openssl
-[ "$FETCH_FROM_GIT" == "true" ] && fetch https://raw.githubusercontent.com/monobilisim/pfsense-5651/master/bin/openssl.cnf -o /logimza/.openssl/openssl.cnf || cp $PWD/bin/openssl.cnf /logimza/.openssl/openssl.cnf
 
-#~ generate new password
-openssl rand -base64 32 > /logimza/.openssl/password.txt
+logsigner_setup() {
+	#~ remove old configs
+	[ "$REM_OLD_CFG" == "true" ] && { echo "Eski yapılandırmalar ve dosyalar siliniyor..."; rm -rf /logimza/.openssl /logimza/$(date +%Y) /usr/local/www/log_browser /sbin/logsigner.sh /sbin/logimza* /sbin/dhcptibduzenle.sh /sbin/dhcpdmodify.awk; } || { [ -e /sbin/logsigner.sh ] && { echo "Logsigner zaten kurulu, yeniden kurmak icin $CONFIGFILE dosyasi icinden REM_OLD_CFG degerini true yapin..."; exit 1; }; }
 
-#~ make requested folders and write serial
-mkdir -p /logimza/.openssl/ssl /logimza/.openssl/CA /logimza/.openssl/CA/private /logimza/.openssl/CA/newcerts
-touch /logimza/.openssl/CA/index.txt
-echo 011E > /logimza/.openssl/CA/serial
-echo 011E > /logimza/.openssl/CA/tsaserial
+	#~ make new folder and write config
+	mkdir -p /logimza/.openssl
+	[ "$FETCH_FROM_GIT" == "true" ] && fetch https://raw.githubusercontent.com/monobilisim/pfsense-5651/master/bin/openssl.cnf -o /logimza/.openssl/openssl.cnf || cp $PWD/bin/openssl.cnf /logimza/.openssl/openssl.cnf
 
-#~ generate CA 
-cd /logimza/.openssl/ssl
-openssl req -config /logimza/.openssl/openssl.cnf -passout file:/logimza/.openssl/password.txt -days 3650 -x509 -newkey rsa:2048 -sha256 -subj "/CN=$SSL_CN/emailAddress=$SSL_EMAIL/O=$SSL_O/C=$SSL_C/ST=$SSL_ST/L=$SSL_L" -out /logimza/.openssl/ssl/cacert.pem -outform PEM
-cp /logimza/.openssl/ssl/cacert.pem /logimza/.openssl/CA/
-cp /logimza/.openssl/ssl/privkey.pem /logimza/.openssl/CA/private/cakey.pem
+	#~ generate new password
+	openssl rand -base64 32 > /logimza/.openssl/password.txt
 
-#~ generate TSA
-openssl genrsa -aes256 -passout file:/logimza/.openssl/password.txt -out /logimza/.openssl/ssl/tsakey.pem 2048
-openssl req -new -config /logimza/.openssl/openssl.cnf -key /logimza/.openssl/ssl/tsakey.pem -passin file:/logimza/.openssl/password.txt -sha256 -subj "/CN=$SSL_CN/emailAddress=$SSL_EMAIL/O=$SSL_O/C=$SSL_C/ST=$SSL_ST/L=$SSL_L" -out /logimza/.openssl/ssl/tsareq.csr
-openssl ca -config /logimza/.openssl/openssl.cnf -passin file:/logimza/.openssl/password.txt -days 3650 -batch -in /logimza/.openssl/ssl/tsareq.csr -subj "/CN=$SSL_CN/emailAddress=$SSL_EMAIL/O=$SSL_O/C=$SSL_C/ST=$SSL_ST/L=$SSL_L" -out /logimza/.openssl/ssl/tsacert.pem
+	#~ make requested folders and write serial
+	mkdir -p /logimza/.openssl/ssl /logimza/.openssl/CA /logimza/.openssl/CA/private /logimza/.openssl/CA/newcerts
+	touch /logimza/.openssl/CA/index.txt
+	echo 011E > /logimza/.openssl/CA/serial
+	echo 011E > /logimza/.openssl/CA/tsaserial
 
-#~ copy new certificates
-cp /logimza/.openssl/ssl/tsacert.pem /logimza/.openssl/CA/
-cp /logimza/.openssl/ssl/tsakey.pem /logimza/.openssl/CA/private/
+	#~ generate CA 
+	cd /logimza/.openssl/ssl
+	openssl req -config /logimza/.openssl/openssl.cnf -passout file:/logimza/.openssl/password.txt -days 3650 -x509 -newkey rsa:2048 -sha256 -subj "/CN=$SSL_CN/emailAddress=$SSL_EMAIL/O=$SSL_O/C=$SSL_C/ST=$SSL_ST/L=$SSL_L" -out /logimza/.openssl/ssl/cacert.pem -outform PEM
+	cp /logimza/.openssl/ssl/cacert.pem /logimza/.openssl/CA/
+	cp /logimza/.openssl/ssl/privkey.pem /logimza/.openssl/CA/private/cakey.pem
+
+	#~ generate TSA
+	openssl genrsa -aes256 -passout file:/logimza/.openssl/password.txt -out /logimza/.openssl/ssl/tsakey.pem 2048
+	openssl req -new -config /logimza/.openssl/openssl.cnf -key /logimza/.openssl/ssl/tsakey.pem -passin file:/logimza/.openssl/password.txt -sha256 -subj "/CN=$SSL_CN/emailAddress=$SSL_EMAIL/O=$SSL_O/C=$SSL_C/ST=$SSL_ST/L=$SSL_L" -out /logimza/.openssl/ssl/tsareq.csr
+	openssl ca -config /logimza/.openssl/openssl.cnf -passin file:/logimza/.openssl/password.txt -days 3650 -batch -in /logimza/.openssl/ssl/tsareq.csr -subj "/CN=$SSL_CN/emailAddress=$SSL_EMAIL/O=$SSL_O/C=$SSL_C/ST=$SSL_ST/L=$SSL_L" -out /logimza/.openssl/ssl/tsacert.pem
+
+	#~ copy new certificates
+	cp /logimza/.openssl/ssl/tsacert.pem /logimza/.openssl/CA/
+	cp /logimza/.openssl/ssl/tsakey.pem /logimza/.openssl/CA/private/
+}
+
+[ -z "$FROM_MONOSPOT" ] && logsigner_setup
 
 #~ install logbrowser
 cd $PROJECT_DIRECTORY
@@ -57,7 +62,7 @@ chmod +x /sbin/logsigner.sh /sbin/dhcpdmodify.awk
 #~ shortcuts
 monospot_entry="\n\t<menu>\n\t\t<name>Monospot</name>\n\t\t<section>Services</section>\n\t\t<url>/monospot</url>\n\t</menu>\n"
 logbrowser_entry="\n\t<menu>\n\t\t<name>5651 Gunluk Tarayicisi</name>\n\t\t<section>Status</section>\n\t\t<url>/logbrowser</url>\n\t</menu>\n"
-[ ! -z "$FROM_MONOSPOT" ] && [ "$MONOSPOT_SHORTCUT" == "true" ] && { echo -e "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<packagegui>${monospot_entry}${logbrowser_entry}</packagegui>" > /usr/local/share/pfSense/menu/pfSense-monospot.xml; }
+[ ! -z "$FROM_MONOSPOT" ] && [ "$MONOSPOT_SHORTCUT" == "true" ] && { echo -e "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<packagegui>${monospot_entry}</packagegui>" > /usr/local/share/pfSense/menu/pfSense-monospot.xml; echo -e "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<packagegui>${logbrowser_entry}</packagegui>" > /usr/local/share/pfSense/menu/pfSense-logbrowser.xml; }
 [ -z "$FROM_MONOSPOT"   ] && [ "$LOGBROWSER_SHORTCUT" == "true" ] && { echo -e "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<packagegui>${logbrowser_entry}</packagegui>" > /usr/local/share/pfSense/menu/pfSense-logbrowser.xml; }
 
 #~ install cron system
